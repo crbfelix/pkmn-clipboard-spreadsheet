120f926a9e3d63a7a1d0f4d7a6c113008cf082c8
N3evin <n3evin@hotmail.com>
2017-04-19 02:16:51 +1000

    Fix '\u2019' error

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from flask import Flask, send_from_directory
 import requests, os
 
@@ -66,7 +64,7 @@ def generateClipboard(key,sheet,row):
         species = species[:-2]
     elif(species.lower() == "farfetch'd"):              # farfetch'd have a special name.
         speciesImage = "farfetchd"
-    elif(species.lower() == "oricorio-pa’u"):           # oricorio-pa’u have a special name.
+    elif('\u2019' in species and "oricorio" in species):# oricorio-pa’u have a special name.
         speciesImage = "oricorio-pa-u"
     elif("." in species.lower()):                       # For Mr. Mine and Mime Jr.
         species = species.title()
